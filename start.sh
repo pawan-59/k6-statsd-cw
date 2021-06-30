da800473627461e8930afeef863a3bcd8bb5472b
root <root@ip-172-31-4-45.ap-south-1.compute.internal>
2021-06-30 19:21:13 +0000

    updated dockerfile

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ touch /root/.aws/credentials
 touch /root/.aws/config
 cp /tmp/credentials /root/.aws/credentials
 cp /tmp/config /root/.aws/config
+cp /tmp/statsd.json /opt/aws/amazon-cloudwatch-agent/bin/default_linux_config.json
 /opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent&
 sleep 10
 K6_STATSD_ENABLE_TAGS=true k6 run --out statsd $1
